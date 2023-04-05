48e4e9b919f22f32a42f708b1e19d61fe4056986
rajeshchaudhary007 <rajeshchaudhary24478546@gmail.com>
2023-04-05 11:58:14 +0545

    initial commit

---
+++ mydb.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'password123'
+
+)
+
+#prepare a cursor object
+cursorObject = dataBase.cursor()
+
+#Create a database 
+cursorObject.execute("CREATE DATABASE crmdb")
+
+
+print("all done!")
